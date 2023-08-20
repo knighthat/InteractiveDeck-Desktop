a09aaafc820b59d2d088e48f75797190f49f98bb
KnightHat <knightchanelgaming@gmail.com>
2023-08-19 22:46:13 -0500

    implemented RequestSerializable

--- src/main/java/me/knighthat/interactivedeck/file/Profile.java
+++ src/main/java/me/knighthat/interactivedeck/file/Profile.java
@@ -13,6 +13,7 @@ package me.knighthat.interactivedeck.file;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import me.knighthat.interactivedeck.WorkingDirectory;
+import me.knighthat.interactivedeck.connection.request.RequestSerializable;
 import me.knighthat.interactivedeck.console.Log;
 import me.knighthat.interactivedeck.exception.ProfileFormatException;
 import me.knighthat.interactivedeck.json.Json;
@@ -27,7 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class Profile implements JsonSerializable {
+public class Profile implements JsonSerializable, RequestSerializable {
 
     private final @NotNull UUID uuid;
     private final @NotNull List<IButton> buttons;
@@ -38,7 +39,7 @@ public class Profile implements JsonSerializable {
 
     private int gap;
 
-    Profile(@NotNull UUID uuid, @NotNull String displayName, boolean isDefault, int columns, int rows, int gap, @NotNull List<IButton> buttons) {
+    Profile( @NotNull UUID uuid, @NotNull String displayName, boolean isDefault, int columns, int rows, int gap, @NotNull List<IButton> buttons ) {
         this.uuid = uuid;
         this.displayName = displayName;
         this.isDefault = isDefault;
@@ -49,43 +50,43 @@ public class Profile implements JsonSerializable {
     }
 
     public Profile() {
-        this(UUID.randomUUID(), "", false, 4, 2, 3, new ArrayList<>());
-        for (int y = 0; y < row(); y++)
-            for (int x = 0; x < column(); x++)
-                this.buttons.add(new IButton(x, y));
+        this( UUID.randomUUID(), "", false, 4, 2, 3, new ArrayList<>() );
+        for ( int y = 0 ; y < row() ; y++ )
+            for ( int x = 0 ; x < column() ; x++ )
+                this.buttons.add( new IButton( this, x, y ) );
     }
 
-    public Profile(@NotNull JsonObject json) {
-        if (!json.has("uuid") ||
-                !json.has("displayName") ||
-                !json.has("default") ||
-                !json.has("rows") ||
-                !json.has("columns") ||
-                !json.has("gap") ||
-                !json.has("buttons"))
-            throw new ProfileFormatException("Missing information");
+    public Profile( @NotNull JsonObject json ) {
+        if ( !json.has( "uuid" ) ||
+                !json.has( "displayName" ) ||
+                !json.has( "default" ) ||
+                !json.has( "rows" ) ||
+                !json.has( "columns" ) ||
+                !json.has( "gap" ) ||
+                !json.has( "buttons" ) )
+            throw new ProfileFormatException( "Missing information" );
 
-        String uuid = json.get("uuid").getAsString();
-        this.uuid = UUID.fromString(uuid);
-        this.displayName = json.get("displayName").getAsString();
-        this.isDefault = json.get("default").getAsBoolean();
-        this.rows = json.get("rows").getAsInt();
-        this.columns = json.get("columns").getAsInt();
-        this.gap = json.get("gap").getAsInt();
+        String uuid = json.get( "uuid" ).getAsString();
+        this.uuid = UUID.fromString( uuid );
+        this.displayName = json.get( "displayName" ).getAsString();
+        this.isDefault = json.get( "default" ).getAsBoolean();
+        this.rows = json.get( "rows" ).getAsInt();
+        this.columns = json.get( "columns" ).getAsInt();
+        this.gap = json.get( "gap" ).getAsInt();
         this.buttons = new ArrayList<>();
 
-        JsonArray btnJson = json.getAsJsonArray("buttons");
-        btnJson.forEach(button -> {
-            IButton btn = IButton.fromJson(button.getAsJsonObject());
-            this.buttons.add(btn);
-        });
+        JsonArray btnJson = json.getAsJsonArray( "buttons" );
+        btnJson.forEach( button -> {
+            IButton btn = IButton.fromJson( this, button.getAsJsonObject() );
+            this.buttons.add( btn );
+        } );
     }
 
     public @NotNull UUID uuid() {
         return this.uuid;
     }
 
-    public void displayName(@NotNull String displayName) {
+    public void displayName( @NotNull String displayName ) {
         this.displayName = displayName;
     }
 
@@ -93,11 +94,11 @@ public class Profile implements JsonSerializable {
         return this.displayName;
     }
 
-    public void column(int columns) {
-        for (int y = 0; y < row(); y++)
-            for (int x = this.columns; x < columns; x++) {
+    public void column( int columns ) {
+        for ( int y = 0 ; y < row() ; y++ )
+            for ( int x = this.columns ; x < columns ; x++ ) {
                 int i = y * x;
-                this.buttons.add(i, new IButton());
+                this.buttons.add( i, new IButton( this, x, y ) );
             }
         this.columns = columns;
     }
@@ -106,11 +107,11 @@ public class Profile implements JsonSerializable {
         return this.columns;
     }
 
-    public void row(int rows) {
-        for (int y = this.rows; y < rows; y++)
-            for (int x = 0; x < column(); x++) {
+    public void row( int rows ) {
+        for ( int y = this.rows ; y < rows ; y++ )
+            for ( int x = 0 ; x < column() ; x++ ) {
                 int i = y * x;
-                this.buttons.add(i, new IButton());
+                this.buttons.add( i, new IButton( this, x, y ) );
             }
         this.rows = rows;
     }
@@ -119,7 +120,7 @@ public class Profile implements JsonSerializable {
         return this.rows;
     }
 
-    public void gap(int gap) {
+    public void gap( int gap ) {
         this.gap = gap;
     }
 
@@ -128,27 +129,26 @@ public class Profile implements JsonSerializable {
     }
 
     public @NotNull @Unmodifiable List<IButton> buttons() {
-        return List.copyOf(this.buttons);
+        return List.copyOf( this.buttons );
     }
 
     public void dump() {
-        Log.info("Saving " + displayName());
+        Log.info( "Saving " + displayName() );
 
-        String fileName = this.uuid.toString().concat(".profile");
-        File file = new File(WorkingDirectory.get(), fileName);
+        String fileName = this.uuid.toString().concat( ".profile" );
+        File file = new File( WorkingDirectory.get(), fileName );
 
         try {
             file.createNewFile();
 
-            Json.save(this.json(), file);
-        } catch (IOException e) {
-            Log.err("Failed to save profile " + displayName());
-            Log.err("Caused by: " + e.getMessage());
+            Json.save( this.json(), file );
+        } catch ( IOException e ) {
+            Log.err( "Failed to save profile " + displayName() );
+            Log.err( "Caused by: " + e.getMessage() );
         }
     }
 
-    @Override
-    public @NotNull JsonObject json() {
+    private @NotNull JsonObject jsonTemplate() {
         /* Template
          * {
          *      "uuid": uuid,
@@ -156,22 +156,43 @@ public class Profile implements JsonSerializable {
          *      "default": isDefault,
          *      "rows": rows,
          *      "columns": columns,
-         *      "gap": gap,
-         *      "buttons":
-         *      [
-         *          buttons
-         *      ]
+         *      "gap": gap
          * }
          */
         JsonObject json = new JsonObject();
 
-        json.add("uuid", Json.parse(this.uuid));
-        json.add("displayName", Json.parse(this.displayName));
-        json.add("default", Json.parse(this.isDefault));
-        json.add("rows", Json.parse(this.row()));
-        json.add("columns", Json.parse(this.column()));
-        json.add("gap", Json.parse(this.gap()));
-        json.add("buttons", Json.parse(this.buttons));
+        json.add( "uuid", Json.parse( this.uuid ) );
+        json.add( "displayName", Json.parse( this.displayName ) );
+        json.add( "default", Json.parse( this.isDefault ) );
+        json.add( "rows", Json.parse( this.row() ) );
+        json.add( "columns", Json.parse( this.column() ) );
+        json.add( "gap", Json.parse( this.gap() ) );
+
+        return json;
+    }
+
+    @Override
+    public @NotNull JsonObject json() {
+        /*
+         * "buttons":
+         * [
+         *      buttons
+         * ]
+         */
+        JsonObject json = jsonTemplate();
+        json.add( "buttons", Json.parse( this.buttons ) );
+
+        return json;
+    }
+
+    @Override
+    public @NotNull JsonObject toRequestFormat() {
+        JsonObject json = jsonTemplate();
+
+        JsonArray buttons = new JsonArray();
+        this.buttons.forEach( btn -> buttons.add( btn.toRequestFormat() ) );
+
+        json.add( "buttons", buttons );
 
         return json;
     }
