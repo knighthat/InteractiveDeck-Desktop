187985930255cf867ce6ebd0087a2571a2091b11
KnightHat <knightchanelgaming@gmail.com>
2023-08-22 08:30:14 -0500

    script executor

---
+++ src/main/java/me/knighthat/interactivedeck/task/BashExecutor.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2023. Knight Hat
+ * All rights reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use,copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package me.knighthat.interactivedeck.task;
+
+import me.knighthat.interactivedeck.console.Log;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public final class BashExecutor {
+
+    private final @NotNull String filePath;
+    private final @NotNull ProcessBuilder process;
+
+    public BashExecutor( @NotNull File file ) {
+        this.filePath = file.getAbsolutePath();
+        this.process = new ProcessBuilder( "bash", filePath );
+    }
+
+    public void execute() {
+        new Thread( () -> {
+            Thread.currentThread().setName( "EXEC" );
+            execute0();
+        } ).start();
+    }
+
+    private void execute0() {
+        int exit = 1;
+        try {
+            Process process = this.process.start();
+            exit = process.waitFor();
+
+            Log.info( "============== Script's output ==============" );
+            this.log( Log.LogLevel.INFO, process.getInputStream() );
+            this.log( Log.LogLevel.WARNING, process.getErrorStream() );
+            Log.info( "----------- End of script output. -----------" );
+
+
+        } catch ( IOException | InterruptedException e ) {
+            //TODO More error handler needed
+            Log.err( "Error occurs while executing " + this.filePath );
+            Log.err( e.getMessage() );
+            e.printStackTrace();
+        }
+
+        String s = exit == 0
+                ? "Script executed successfully!"
+                : "Script execution failed with exit code: " + exit;
+        Log.log( exit == 0 ? Log.LogLevel.INFO : Log.LogLevel.WARNING, s );
+    }
+
+    private void log( @NotNull Log.LogLevel level, @NotNull InputStream stream ) throws IOException {
+        int eightBytes = 8 * 1024;
+        byte[] buffer = new byte[eightBytes];
+        int bytesRead;
+
+        while ( ( bytesRead = stream.read( buffer ) ) != -1 ) {
+            String decoded = new String( buffer, 0, bytesRead );
+            Log.log( level, decoded );
+        }
+    }
+}
