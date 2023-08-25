8dc479e0d76abc518bc3a89180bc6295ad2af1e6
KnightHat <knightchanelgaming@gmail.com>
2023-08-25 18:10:50 -0500

    fixed task method did not accept null value

--- src/main/java/me/knighthat/interactivedeck/menus/component/ibutton/IButton.java
+++ src/main/java/me/knighthat/interactivedeck/menus/component/ibutton/IButton.java
@@ -39,12 +39,12 @@ public class IButton extends JComponent implements JsonSerializable, RequestSeri
     private final int y;
     private @Nullable Task task;
 
-    IButton( @NotNull UUID uuid,
-             @NotNull Profile profile,
-             @NotNull BIcon icon,
-             @NotNull BLabel label,
-             int x, int y,
-             @Nullable Task task ) {
+    IButton(@NotNull UUID uuid,
+            @NotNull Profile profile,
+            @NotNull BIcon icon,
+            @NotNull BLabel label,
+            int x, int y,
+            @Nullable Task task) {
         this.uuid = uuid;
         this.profile = profile;
         this.icon = icon;
@@ -53,50 +53,52 @@ public class IButton extends JComponent implements JsonSerializable, RequestSeri
         this.y = y;
         this.task = task;
 
-        setOpaque( false );
-        setForeground( TRANSPARENT );
+        setOpaque(false);
+        setForeground(TRANSPARENT);
 
-        setLayout( new OverlayLayout( this ) );
-        add( label, 0 );
-        add( icon, 1 );
+        setLayout(new OverlayLayout(this));
+        add(label, 0);
+        add(icon, 1);
     }
 
-    public IButton( @NotNull Profile profile, int x, int y ) {
-        this( UUID.randomUUID(), profile, new BIcon(), new BLabel(), x, y, null );
+    public IButton(@NotNull Profile profile, int x, int y) {
+        this(UUID.randomUUID(), profile, new BIcon(), new BLabel(), x, y, null);
     }
 
 
-    public static @NotNull IButton fromJson( @NotNull Profile profile, @NotNull JsonObject json ) throws JsonSyntaxException {
-        if ( !json.has( "uuid" ) ||
-                !json.has( "icon" ) ||
-                !json.has( "label" ) )
-            throw new JsonSyntaxException( "Not enough argument" );
+    public static @NotNull IButton fromJson(@NotNull Profile profile, @NotNull JsonObject json) throws JsonSyntaxException {
+        if (!json.has("uuid") ||
+                !json.has("icon") ||
+                !json.has("label"))
+            throw new JsonSyntaxException("Not enough argument");
 
-        String idStr = json.get( "uuid" ).getAsString();
-        UUID uuid = UUID.fromString( idStr );
+        String idStr = json.get("uuid").getAsString();
+        UUID uuid = UUID.fromString(idStr);
 
-        JsonObject iconJson = json.getAsJsonObject( "icon" );
-        BIcon icon = BIcon.fromJson( iconJson );
+        JsonObject iconJson = json.getAsJsonObject("icon");
+        BIcon icon = BIcon.fromJson(iconJson);
 
-        JsonObject labelJson = json.getAsJsonObject( "label" );
-        BLabel label = BLabel.fromJson( labelJson );
+        JsonObject labelJson = json.getAsJsonObject("label");
+        BLabel label = BLabel.fromJson(labelJson);
 
-        JsonPrimitive xPrim = json.getAsJsonPrimitive( "x" );
+        JsonPrimitive xPrim = json.getAsJsonPrimitive("x");
         int x = xPrim.getAsInt();
 
-        JsonPrimitive yPrim = json.getAsJsonPrimitive( "y" );
+        JsonPrimitive yPrim = json.getAsJsonPrimitive("y");
         int y = yPrim.getAsInt();
 
-        Task task = json.has( "task" ) ? Task.fromJson( json.getAsJsonObject( "task" ) ) : null;
+        Task task = json.has("task") ? Task.fromJson(json.getAsJsonObject("task")) : null;
 
-        return new IButton( uuid, profile, icon, label, x, y, task );
+        return new IButton(uuid, profile, icon, label, x, y, task);
     }
 
     public @NotNull UUID uuid() {
         return this.uuid;
     }
 
-    public @NotNull Profile profile() { return this.profile; }
+    public @NotNull Profile profile() {
+        return this.profile;
+    }
 
     public int x() {
         return this.x;
@@ -107,38 +109,38 @@ public class IButton extends JComponent implements JsonSerializable, RequestSeri
     }
 
     public void select() {
-        this.icon.repaint( SELECTED_COLOR, this.icon.inner() );
+        this.icon.repaint(SELECTED_COLOR, this.icon.inner());
     }
 
     public void unselect() {
-        this.icon.repaint( this.icon.inner(), this.icon.inner() );
+        this.icon.repaint(this.icon.inner(), this.icon.inner());
     }
 
-    public void background( @NotNull Color bg ) {
-        this.icon.repaint( null, bg );
+    public void background(@NotNull Color bg) {
+        this.icon.repaint(null, bg);
     }
 
     public @NotNull Color background() {
         return this.icon.inner();
     }
 
-    public void foreground( @NotNull Color fg ) {
-        this.label.setForeground( fg );
+    public void foreground(@NotNull Color fg) {
+        this.label.setForeground(fg);
     }
 
     public @NotNull Color foreground() {
         return this.label.getForeground();
     }
 
-    public void text( @NotNull String text ) {
-        this.label.text( text );
+    public void text(@NotNull String text) {
+        this.label.text(text);
     }
 
     public @NotNull String text() {
         return this.label.text();
     }
 
-    public void task( @NotNull Task task ) {
+    public void task(@Nullable Task task) {
         this.task = task;
     }
 
@@ -166,13 +168,13 @@ public class IButton extends JComponent implements JsonSerializable, RequestSeri
          */
         JsonObject json = new JsonObject();
 
-        json.add( "uuid", Json.parse( this.uuid ) );
-        json.add( "x", Json.parse( this.x ) );
-        json.add( "y", Json.parse( this.y ) );
-        if ( this.task != null )
-            json.add( "task", this.task.json() );
-        json.add( "icon", this.icon.json() );
-        json.add( "label", this.label.json() );
+        json.add("uuid", Json.parse(this.uuid));
+        json.add("x", Json.parse(this.x));
+        json.add("y", Json.parse(this.y));
+        if (this.task != null)
+            json.add("task", this.task.json());
+        json.add("icon", this.icon.json());
+        json.add("label", this.label.json());
 
         return json;
     }
@@ -191,14 +193,14 @@ public class IButton extends JComponent implements JsonSerializable, RequestSeri
          */
         JsonObject json = new JsonObject();
 
-        json.add( "uuid", Json.parse( this.uuid ) );
-        json.add( "x", Json.parse( this.x ) );
-        json.add( "y", Json.parse( this.y ) );
-        json.add( "background", Json.parse( this.icon.inner() ) );
-        json.add( "foreground", Json.parse( this.label.getForeground() ) );
-        json.add( "text", Json.parse( this.label.text() ) );
-        if ( task instanceof GotoPage t )
-            json.add( "goto", Json.parse( t.target().toString() ) );
+        json.add("uuid", Json.parse(this.uuid));
+        json.add("x", Json.parse(this.x));
+        json.add("y", Json.parse(this.y));
+        json.add("background", Json.parse(this.icon.inner()));
+        json.add("foreground", Json.parse(this.label.getForeground()));
+        json.add("text", Json.parse(this.label.text()));
+        if (task instanceof GotoPage t)
+            json.add("goto", Json.parse(t.target().toString()));
 
         return json;
     }
