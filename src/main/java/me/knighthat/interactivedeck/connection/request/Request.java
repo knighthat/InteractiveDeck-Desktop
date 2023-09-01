8bb3fb63535f2eaa987b12bfbfad9613069369f0
KnightHat <knightchanelgaming@gmail.com>
2023-09-01 18:23:40 -0500

    merged RequestType to Request

--- src/main/java/me/knighthat/interactivedeck/connection/request/Request.java
+++ src/main/java/me/knighthat/interactivedeck/connection/request/Request.java
@@ -1,11 +1,15 @@
 /*
  * Copyright (c) 2023. Knight Hat
  * All rights reserved.
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use,copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use,copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package me.knighthat.interactivedeck.connection.request;
@@ -26,13 +30,13 @@ public class Request {
     }
 
     public static @NotNull Request parse( @NotNull JsonObject json ) {
-        String typeStr = json.get("type").getAsString();
-        RequestType type = RequestType.valueOf(typeStr);
+        String typeStr = json.get( "type" ).getAsString();
+        RequestType type = RequestType.valueOf( typeStr );
 
-        boolean hasContent = json.has("content");
-        JsonElement content = !hasContent ? JsonNull.INSTANCE : json.get("content");
+        boolean hasContent = json.has( "content" );
+        JsonElement content = !hasContent ? JsonNull.INSTANCE : json.get( "content" );
 
-        return new Request(type, content);
+        return new Request( type, content );
     }
 
     public @NotNull RequestType type() {
@@ -45,9 +49,13 @@ public class Request {
 
     public @NotNull String serialize() {
         JsonObject json = new JsonObject();
-        json.add("type", type().json());
-        json.add("content", content());
+        json.addProperty( "type", type.toString() );
+        json.add( "content", content() );
 
         return json.toString();
+    }
+
+    public enum RequestType {
+        ADD, REMOVE, UPDATE, PAIR, ACTION
     }
 }
