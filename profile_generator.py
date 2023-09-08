72a39a8f3f22bce518b8adae39da775b5c607605
KnightHat <knightchanelgaming@gmail.com>
2023-09-08 15:48:37 -0500

    improved readability & updated to the latest profile format

--- profile_generator.py
+++ profile_generator.py
@@ -1,8 +1,23 @@
+#  Copyright (c) 2023. Knight Hat
+#  All rights reserved.
+#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use,copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 from json import dumps
-from random import randrange, randint, choice
+from random import randrange, choice
 from string import ascii_letters
 from sys import argv
 from uuid import uuid4
+
+fonts = ["Arial", "Times New Roman", "Stardos Stencil"]
+weights = ["plain", "bold", "italic", "bold|italic"]
+
+
+def uuid() -> str:
+    return str(uuid4())
 
 
 def randcolor() -> list[int]:
@@ -10,61 +25,54 @@ def randcolor() -> list[int]:
 
 
 def randstr(r: int) -> str:
-    return ''.join(choice(ascii_letters) for i in range(r))
+    return ''.join(choice(ascii_letters) for _ in range(r))
 
 
-def uuidstr() -> str:
-    return str(uuid4())
+class Font:
+    def __init__(self):
+        self.name = choice(fonts)
+        self.weight = choice(weights)
+        self.size = randrange(15) + 1
 
 
-def button(x: int, y: int) -> dict:
-    return {
-        "uuid": uuidstr(),
-        "x": x,
-        "y": y,
-        "icon": {
-            "outer": randcolor(),
-            "inner": randcolor()
-        },
-        "label": {
-            "text": randstr(5),
-            "color": randcolor(),
-            "font": {
-                "name": "Stardos Stencil",
-                "weight": "plain",
-                "size": 14
-            }
-        }
-    }
+class BLabel:
+    def __init__(self):
+        self.text = randstr(5)
+        self.foreground = randcolor()
+        self.font = Font()
 
 
-def generate() -> dict:
-    uuid = uuidstr()
+class Button:
+    def __init__(self, x: int, y: int):
+        self.uuid = uuid()
+        self.x = x
+        self.y = y
+        self.task = None
+        self.icon = {"background": randcolor(), "border": randcolor()}
+        self.label = BLabel()
 
-    rows = randint(1, 4)
-    columns = randint(1, 6)
 
-    buttons = []
+class Profile:
+    def __init__(self):
+        self.uuid = uuid()
+        self.displayName = randstr(10)
+        self.default = False
+        self.rows = randrange(4) + 1
+        self.columns = randrange(6) + 1
+        self.gap = randrange(10)
+        self.buttons: list[Button] = []
 
-    for y in range(rows):
-        for x in range(columns):
-            buttons.append(button(x, y))
+        for y in range(self.rows):
+            for x in range(self.columns):
+                self.buttons.append(Button(x, y))
 
-    return {
-        "uuid": uuid,
-        "displayName": randstr(10),
-        "default": False,
-        "rows": rows,
-        "columns": columns,
-        "gap": randrange(4),
-        "buttons": buttons
-    }
+    def __json__(self) -> str:
+        return dumps(self, default=lambda btn: btn.__dict__, indent=2)
 
 
 if __name__ == '__main__':
     times = 1 if len(argv) == 1 else int(argv[1])
-    for i in range(times):
-        jsonObject = generate()
-        encodedJson = dumps(jsonObject, indent=2)
-        with open(f'{jsonObject["uuid"]}.profile', 'w') as f:
-            f.write(encodedJson)
+    for _ in range(times):
+        profile = Profile()
+        with open(f'{profile.uuid}.profile', 'w') as p:
+            p.write(profile.__json__())
