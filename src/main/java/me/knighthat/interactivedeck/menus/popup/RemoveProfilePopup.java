532eecf1c8b954bf57462f5b0ec8acd9c3af1906
KnightHat <knightchanelgaming@gmail.com>
2023-09-13 20:44:19 -0500

    confirmation popup when user try to remove a profile

---
+++ src/main/java/me/knighthat/interactivedeck/menus/popup/RemoveProfilePopup.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2023. Knight Hat
+ * All rights reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use,copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package me.knighthat.interactivedeck.menus.popup;
+
+import me.knighthat.interactivedeck.file.Profile;
+import me.knighthat.interactivedeck.menus.MainMenu;
+import me.knighthat.interactivedeck.menus.MenuProperty;
+import me.knighthat.interactivedeck.utils.UuidUtils;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+
+public class RemoveProfilePopup extends ProfilePopup {
+
+    private JLabel message;
+
+    public RemoveProfilePopup( @NotNull Window window ) {
+        super( window, "Remove Profile", "Remove", "Cancel" );
+    }
+
+    @Override
+    protected void loadContent() {
+        addContent(
+                new JLabel(),
+                label -> {
+                    message = (JLabel) label;
+                    setDimension( label, 250, 50 );
+                },
+                constraints -> {}
+        );
+    }
+
+    @Override
+    protected void loadProfile( @NotNull Profile profile ) {
+        String msg = "<html>You are about to remove profile %s (%s). Do you want to continue?</html>";
+        String uuid = UuidUtils.lastFiveChars( profile.uuid );
+        message.setText( msg.formatted( profile.displayName(), uuid ) );
+    }
+
+    @Override
+    protected void positiveButtonClickEvent( @NotNull MouseEvent event ) {
+        profile.remove();
+        ( (MainMenu) getOwner() ).updateProfilesList();
+        MenuProperty.active( MenuProperty.defaultProfile() );
+    }
+}
