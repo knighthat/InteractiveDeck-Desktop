df44076746b21acdeeeabfacd4baedc91e9bee8b
KnightHat <knightchanelgaming@gmail.com>
2023-09-15 14:46:53 -0500

    replaced SVG with new icon & better algorithm to ensure non-null

--- src/main/java/me/knighthat/interactivedeck/svg/SVGNotFound.java
+++ src/main/java/me/knighthat/interactivedeck/svg/SVGNotFound.java
@@ -14,28 +14,38 @@
 
 package me.knighthat.interactivedeck.svg;
 
+import org.apache.batik.anim.dom.SVGDOMImplementation;
 import org.jetbrains.annotations.NotNull;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Element;
 import org.w3c.dom.svg.SVGDocument;
+import org.w3c.dom.svg.SVGSVGElement;
 
 public class SVGNotFound {
 
-    public static final SVGDocument DOCUMENT;
+    public static final @NotNull SVGDocument DOCUMENT;
 
     /*
-     * This string is licensed under CC BY 4.0 - https://creativecommons.org/licenses/by/4.0/
-     * https://www.svgrepo.com/svg/441689/page-not-found
+     * This string is licensed under MIT - https://mit-license.org/
+     * https://www.svgrepo.com/svg/332374/file-unknown
      */
-    private static final @NotNull String D = "M38.155 140.475L48.988 62.1108L92.869 67.0568L111.437 91.0118L103.396 148.121L38.155 140.475ZM84.013 94.0018L88.827 71.8068L54.046 68.3068L44.192 135" +
-            ".457L98.335 142.084L104.877 96.8088L84.013 94.0018ZM59.771 123.595C59.394 123.099 56.05 120.299 55.421 119.433C64.32 109.522 86.05 109.645 92.085 122.757C91.08 123.128 86.59 125.072 " +
-            "85.71 125.567C83.192 118.25 68.445 115.942 59.771 123.595ZM76.503 96.4988L72.837 99.2588L67.322 92.6168L59.815 96.6468L56.786 91.5778L63.615 88.1508L59.089 82.6988L64.589 79.0188L68" +
-            ".979 85.4578L76.798 81.5328L79.154 86.2638L72.107 90.0468L76.503 96.4988Z";
+    private static final @NotNull String D = "M854.6 288.7L639.4 73.4c-6-6-14.2-9.4-22.7-9.4H192c-17.7 0-32 14.3-32 32v832c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 " +
+            "32-32V311.3c0-8.5-3.4-16.6-9.4-22.6zM790.2 326H602V137.8L790.2 326zm1.8 562H232V136h302v216a42 42 0 0 0 42 42h216v494zM402 549c0 5.4 4.4 9.5 9.8 9.5h32.4c5.4 0 9.8-4.2 9.8-9.4 0-28.2 " +
+            "25.8-51.6 58-51.6s58 23.4 58 51.5c0 25.3-21 47.2-49.3 50.9-19.3 2.8-34.5 20.3-34.7 40.1v32c0 5.5 4.5 10 10 10h32c5.5 0 10-4.5 10-10v-12.2c0-6 4-11.5 9.7-13.3 44.6-14.4 75-54 " +
+            "74.3-98.9-.8-55.5-49.2-100.8-108.5-101.6-61.4-.7-111.5 45.6-111.5 103zm78 195a32 32 0 1 0 64 0 32 32 0 1 0-64 0z";
 
     static {
-        String header = "<svg viewBox=\"-20 0 190 190\" xmlns=\"http://www.w3.org/2000/svg\">";
-        String footer = "</svg>";
-        String path = "<path d=\"%s\" fill=\"#000000\"/>".formatted( D );
+        DOMImplementation implementation = SVGDOMImplementation.getDOMImplementation();
 
-        String formatted = "%s%s%s".formatted( header, path, footer );
-        DOCUMENT = SVGParser.fromString( formatted ).get();
+        String namespaceURI = SVGDOMImplementation.SVG_NAMESPACE_URI;
+        DOCUMENT = (SVGDocument) implementation.createDocument( namespaceURI, "svg", null );
+
+        SVGSVGElement root = DOCUMENT.getRootElement();
+        root.setAttribute( "viewBox", "0 0 1024 1024" );
+
+        Element path = DOCUMENT.createElementNS( namespaceURI, "path" );
+        path.setAttribute( "d", D );
+        path.setAttribute( "fill", "#000000" );
+        root.appendChild( path );
     }
 }
