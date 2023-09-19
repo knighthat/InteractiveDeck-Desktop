18d8ab52e1494cb9746d424c77f61b9f51f19bf0
KnightHat <knightchanelgaming@gmail.com>
2023-09-19 11:59:31 -0500

    added class to display settings icon

---
+++ src/main/java/me/knighthat/interactivedeck/component/setting/SettingButton.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2023. Knight Hat
+ * All rights reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use,copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package me.knighthat.interactivedeck.component.setting;
+
+import me.knighthat.interactivedeck.component.icon.Icons;
+import org.apache.batik.swing.JSVGCanvas;
+import org.jetbrains.annotations.Nullable;
+import org.w3c.dom.svg.SVGDocument;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import static me.knighthat.interactivedeck.utils.ColorUtils.TRANSPARENT;
+
+public class SettingButton extends JSVGCanvas {
+
+    private final @Nullable SVGDocument ICON;
+    private final @Nullable SVGDocument ICON_HOVER;
+
+    public SettingButton() {
+        ICON = Icons.INTERNAL.APP_SETTINGS;
+        ICON_HOVER = Icons.INTERNAL.APP_SETTINGS_HOVER;
+
+        setBackground( TRANSPARENT );
+        setDocument( ICON );
+        setRecenterOnResize( false );
+
+        addMouseListener( new MouseAdapter() {
+            @Override
+            public void mouseEntered( MouseEvent e ) {
+                setDocument( ICON_HOVER );
+            }
+
+            @Override
+            public void mouseExited( MouseEvent e ) {
+                setDocument( ICON );
+            }
+        } );
+    }
+}
