cdce28daee41f9e038a7f17ffe38e174d1bb86a0
KnightHat <knightchanelgaming@gmail.com>
2023-10-30 19:36:06 -0500

    removed null task & wrapped copyright notice

--- profile_generator.py
+++ profile_generator.py
@@ -6,6 +6,13 @@
 #
 #  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use,copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#
 from json import dumps
 from random import randrange, choice
 from string import ascii_letters
@@ -47,7 +54,6 @@ class Button:
         self.uuid = uuid()
         self.x = x
         self.y = y
-        self.task = None
         self.icon = {"background": randcolor(), "border": randcolor()}
         self.label = BLabel()
 
