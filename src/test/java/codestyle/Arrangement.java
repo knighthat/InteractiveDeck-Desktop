d8b00e8a1d77af3fec43b44d0871d41c65c4e855
KnightHat <knightchanelgaming@gmail.com>
2023-11-15 22:45:15 -0600

    updated to new format

--- src/test/java/codestyle/Arrangement.java
+++ src/test/java/codestyle/Arrangement.java
@@ -1,20 +1,18 @@
 /*
  * Copyright (c) 2023. Knight Hat
  * All rights reserved.
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use,copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use,copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
- * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package codestyle;
 
 import me.knighthat.interactivedeck.component.input.HexColorTextField;
+
+// START: Subclass
 
 /**
  * General rules:
@@ -24,24 +22,25 @@ import me.knighthat.interactivedeck.component.input.HexColorTextField;
  * - Inner enums/interfaces/classes are located at the bottom with above rules applied.<br>
  * - Overridden methods must be kept in order.<br>
  */
-@SuppressWarnings( { "unused", "InnerClassMayBeStatic", "EmptyClassInitializer" } )
+@SuppressWarnings({ "unused", "InnerClassMayBeStatic", "EmptyClassInitializer" })
 public abstract class Arrangement {
 
+    // START: Static fields/functions
     /*
      * Static final fields
      */
-    public static final Object PUB_FIN_OBJ = new Object();
+    public static final    Object PUB_FIN_OBJ = new Object();
     protected static final Object PRO_FIN_OBJ = new Object();
-    private static final Object PRI_FIN_OBJ = new Object();
-    static final Object STA_FIN_OBJ = new Object();
+    private static final   Object PRI_FIN_OBJ = new Object();
+    static final           Object STA_FIN_OBJ = new Object();
 
     /*
      * Static non-final fields
      */
-    public static Object PUB_STA_OBJ;
+    public static    Object PUB_STA_OBJ;
     protected static Object PRO_STA_OBJ;
-    private static Object PRI_STA_OBJ;
-    static Object STA_OBJ;
+    private static   Object PRI_STA_OBJ;
+    static           Object STA_OBJ;
 
     /*
      * Static initializer
@@ -59,20 +58,21 @@ public abstract class Arrangement {
     protected static void pro_sta_me() {}
 
     public static void pub_sta_me() {}
+// END: Static fields/functions
 
     /*
      * Final fields
      */
-    public final Object pub_fin_obj = new Object();
+    public final    Object pub_fin_obj = new Object();
     protected final Object pro_fin_obj = new Object();
-    private final Object pri_fin_obj = new Object();
-    final Object fin_obj = new Object();
+    private final   Object pri_fin_obj = new Object();
+    final           Object fin_obj     = new Object();
     /*
      * Non-final fields
      */
-    public Object pub_obj;
-    protected Object pro_obj;
-    private Object pri_obj;
+    public          Object pub_obj;
+    protected       Object pro_obj;
+    private         Object pri_obj;
     Object obj;
 
     /*
